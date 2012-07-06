c3b4cf899ae81d4c8a967f9f4896151e40b3b9c2
Eric Barr <eb@anhstudios.com>
2012-07-06 07:29:18 -0400

    Removed exploritory code

---
+++ src/swganh/object/object_template_manager.cc
@@ -0,0 +1,9 @@
+
+#include "object_template_manager.h"
+
+#include "anh/resource/resource_manager.h"
+
+#include "object_template.h"
+
+using swganh::object::ObjectTemplate;
+using swganh::object::ObjectTemplateManager;
---
+++ src/swganh/object/object_template_manager.h
@@ -0,0 +1,25 @@
+// This file is part of SWGANH which is released under the MIT license.
+// See file LICENSE or go to http://swganh.com/LICENSE
+
+#ifndef SWGANH_OBJECT_OBJECT_TEMPLATE_MANAGER_H_
+#define SWGANH_OBJECT_OBJECT_TEMPLATE_MANAGER_H_
+
+#include <string>
+#include <unordered_map>
+
+namespace anh {
+namespace resource {
+    class ResourceManager;
+}}
+
+namespace swganh {
+namespace object {
+
+    class ObjectTemplate;
+
+    class ObjectTemplateManager
+    {};
+
+}}
+
+#endif
