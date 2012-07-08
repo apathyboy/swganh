984f5958cfae100e20e12581e143c6435a280a83
Eric Barr <eb@anhstudios.com>
2012-07-08 08:45:48 -0400

    Refactored integer types into a template base

--- src/anh/property/property_types.cc
+++ src/anh/property/property_types.cc
@@ -5,134 +5,197 @@
 
 #include <sstream>
 
-#pragma warning( push )
-#pragma warning( disable : 4244 )
-#include <boost/archive/binary_iarchive.hpp>
-#include <boost/archive/binary_oarchive.hpp>
-#pragma warning( pop )
+#include "anh/utilities.h"
 
 using namespace anh::property;
 
 
 Int8Property::Int8Property(
-    std::string name, 
-    std::string label, 
-    std::string description, 
+    std::string name,
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<int8_t>(P_INT8, name, label, description, group, std::move(getter), std::move(setter))
+    : IntegerProperty<int8_t>(P_INT8, name, group, std::move(getter), std::move(setter))
 {}
 
 Int16Property::Int16Property(
-    std::string name, 
-    std::string label, 
-    std::string description, 
+    std::string name,
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<int16_t>(P_INT16, name, label, description, group, std::move(getter), std::move(setter))
+    : IntegerProperty<int16_t>(P_INT16, name, group, std::move(getter), std::move(setter))
 {}
 
 Int32Property::Int32Property(
-    std::string name, 
-    std::string label, 
-    std::string description, 
+    std::string name,
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<int32_t>(P_INT32, name, label, description, group, std::move(getter), std::move(setter))
+    : IntegerProperty<int32_t>(P_INT32, name, group, std::move(getter), std::move(setter))
 {}
 
 Int64Property::Int64Property(
-    std::string name, 
-    std::string label, 
-    std::string description, 
+    std::string name,
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<int64_t>(P_INT64, name, label, description, group, std::move(getter), std::move(setter))
+    : IntegerProperty<int64_t>(P_INT64, name, group, std::move(getter), std::move(setter))
 {}
 
-
 UInt8Property::UInt8Property(
-    std::string name, 
-    std::string label, 
-    std::string description, 
+    std::string name,
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<uint8_t>(P_UINT8, name, label, description, group, std::move(getter), std::move(setter))
+    : IntegerProperty<uint8_t>(P_UINT8, name, group, std::move(getter), std::move(setter))
 {}
 
 UInt16Property::UInt16Property(
-    std::string name, 
-    std::string label, 
-    std::string description, 
+    std::string name,
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<uint16_t>(P_UINT16, name, label, description, group, std::move(getter), std::move(setter))
+    : IntegerProperty<uint16_t>(P_UINT16, name, group, std::move(getter), std::move(setter))
 {}
 
 UInt32Property::UInt32Property(
-    std::string name, 
-    std::string label, 
-    std::string description, 
+    std::string name,
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<uint32_t>(P_UINT32, name, label, description, group, std::move(getter), std::move(setter))
+    : IntegerProperty<uint32_t>(P_UINT32, name, group, std::move(getter), std::move(setter))
 {}
 
 UInt64Property::UInt64Property(
-    std::string name, 
-    std::string label, 
-    std::string description, 
+    std::string name,
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<uint64_t>(P_UINT64, name, label, description, group, std::move(getter), std::move(setter))
+    : IntegerProperty<uint64_t>(P_UINT64, name, group, std::move(getter), std::move(setter))
 {}
 
 FloatProperty::FloatProperty(
-    std::string name, 
-    std::string label, 
-    std::string description, 
+    std::string name,
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<float>(P_FLOAT, name, label, description, group, std::move(getter), std::move(setter))
+    : BaseProperty<float>(P_FLOAT, name, group, std::move(getter), std::move(setter))
 {}
+
+std::string FloatProperty::DoToString() const
+{
+    return std::to_string(GetValue());
+}
+
+void FloatProperty::DoFromString(const std::string& string_value)
+{
+    SetValue(boost::lexical_cast<float>(string_value));
+}
+
+size_t FloatProperty::DoSerialize(boost::archive::binary_oarchive& archive) const
+{
+    return 0;
+}
+
+void FloatProperty::DoDeserialize(boost::archive::binary_iarchive& archive, size_t length)
+{
+    uint8_t enabled;
+    archive >> enabled;
+
+    if (enabled > 0)
+    {
+        uint8_t spacer;
+        float value;
+
+        archive >> spacer;
+        archive >> value;
+
+        SetValue(value);
+    }
+}
 
 BoolProperty::BoolProperty(
-    std::string name, 
-    std::string label, 
-    std::string description, 
+    std::string name,
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<bool>(P_BOOL, name, label, description, group, std::move(getter), std::move(setter))
+    : BaseProperty<bool>(P_BOOL, name, group, std::move(getter), std::move(setter))
 {}
+
+std::string BoolProperty::DoToString() const
+{
+    return std::to_string(GetValue());
+}
+
+void BoolProperty::DoFromString(const std::string& string_value)
+{
+    SetValue(boost::lexical_cast<bool>(string_value));
+}
+
+size_t BoolProperty::DoSerialize(boost::archive::binary_oarchive& archive) const
+{
+    return 0;
+}
+
+void BoolProperty::DoDeserialize(boost::archive::binary_iarchive& archive, size_t length)
+{
+    uint8_t enabled;
+    archive >> enabled;
+
+    if (enabled > 0)
+    {
+        uint8_t value;
+
+        archive >> value;
+
+        SetValue(value != 0);
+    }
+}
 
 StringProperty::StringProperty(
-    std::string name, 
-    std::string label, 
-    std::string description, 
+    std::string name,
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<std::string>(P_BOOL, name, label, description, group, std::move(getter), std::move(setter))
+    : BaseProperty<std::string>(P_BOOL, name, group, std::move(getter), std::move(setter))
 {}
 
+std::string StringProperty::DoToString() const
+{
+    return GetValue();
+}
+
+void StringProperty::DoFromString(const std::string& string_value)
+{
+    SetValue(string_value);
+}
+
+size_t StringProperty::DoSerialize(boost::archive::binary_oarchive& archive) const
+{
+    return 0;
+}
+
+void StringProperty::DoDeserialize(boost::archive::binary_iarchive& archive, size_t length)
+{
+    uint8_t enabled;
+    archive >> enabled;
+
+    if (enabled > 0)
+    {
+        std::string value;
+
+        archive >> value;
+
+        SetValue(value);
+    }
+}
+
 StringListProperty::StringListProperty(
-    std::string name, 
-    std::string label, 
-    std::string description, 
+    std::string name,
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<StringList>(P_BOOL, name, label, description, group, std::move(getter), std::move(setter))
+    : BaseProperty<StringList>(P_BOOL, name, group, std::move(getter), std::move(setter))
 {}
 
 StringListProperty::~StringListProperty()
@@ -153,9 +216,7 @@ std::string StringListProperty::DoToString() const
 }
 
 void StringListProperty::DoFromString(const std::string& string_value)
-{
-
-}
+{}
 
 size_t StringListProperty::DoSerialize(boost::archive::binary_oarchive& archive) const
 {
@@ -189,43 +250,35 @@ void StringListProperty::DoDeserialize(boost::archive::binary_iarchive& archive,
 }
 
 UStringProperty::UStringProperty(
-    std::string name, 
-    std::string label, 
-    std::string description, 
+    std::string name,
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<std::wstring>(P_BOOL, name, label, description, group, std::move(getter), std::move(setter))
+    : BaseProperty<std::wstring>(P_BOOL, name, group, std::move(getter), std::move(setter))
 {}
 
 Vector2Property::Vector2Property(
-    std::string name, 
-    std::string label, 
-    std::string description, 
+    std::string name,
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<glm::vec2>(P_BOOL, name, label, description, group, std::move(getter), std::move(setter))
+    : BaseProperty<glm::vec2>(P_BOOL, name, group, std::move(getter), std::move(setter))
 {}
 
 Vector3Property::Vector3Property(
-    std::string name, 
-    std::string label, 
-    std::string description, 
+    std::string name,
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<glm::vec3>(P_BOOL, name, label, description, group, std::move(getter), std::move(setter))
+    : BaseProperty<glm::vec3>(P_BOOL, name, group, std::move(getter), std::move(setter))
 {}
 
 QuaternionProperty::QuaternionProperty(
     std::string name, 
-    std::string label, 
-    std::string description, 
     std::string group, 
     Getter&& getter, 
     Setter&& setter)
-    : BaseProperty<glm::quat>(P_BOOL, name, label, description, group, std::move(getter), std::move(setter))
+    : BaseProperty<glm::quat>(P_BOOL, name, group, std::move(getter), std::move(setter))
 {}
 
 
--- src/anh/property/property_types.h
+++ src/anh/property/property_types.h
@@ -7,8 +7,19 @@
 #include "property.h"
 
 #include <list>
+
+#pragma warning( push )
+#pragma warning( disable : 4244 )
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#pragma warning( pop )
+
+#include <boost/lexical_cast.hpp>
+
 #include <glm/glm.hpp>
 #include <glm/gtx/quaternion.hpp>
+
+#include "anh/utilities.h"
 
 namespace anh {
 namespace property {
@@ -35,68 +46,99 @@ namespace property {
         P_STRING_LIST
     };
 
-    class Int8Property : public BaseProperty<int8_t>
+    template<typename T>
+    class IntegerProperty : public BaseProperty<T>
     {
     public:
-        typedef int8_t property_type;
+        typedef T property_type;
 
-        Int8Property(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+        IntegerProperty(uint32_t type, std::string name, std::string group, Getter&& getter, Setter&& setter)
+            : BaseProperty<T>(type, name, group, std::move(getter), std::move(setter))
+        {}
+
+    private:
+        virtual std::string DoToString() const
+        {
+            return std::to_string(GetValue());
+        }
+
+        virtual void DoFromString(const std::string& string_value)
+        {
+            SetValue(boost::lexical_cast<T>(string_value));
+        }
+    
+        virtual size_t DoSerialize(boost::archive::binary_oarchive& archive) const
+        {
+            //archive << uint8_t(1);
+            //archive << " ";
+            //archive << anh::hostToBig(GetValue());
+            return 0;
+        }
+
+        virtual void DoDeserialize(boost::archive::binary_iarchive& archive, size_t length)
+        {
+            uint8_t enabled;
+            archive >> enabled;
+
+            if (enabled > 0)
+            {
+                uint8_t spacer;
+                T value;
+
+                archive >> spacer;
+                archive >> value;
+
+                SetValue(anh::bigToHost(value));
+            }
+        }
     };
 
-    class Int16Property : public BaseProperty<int16_t>
+    class Int8Property : public IntegerProperty<int8_t>
     {
     public:
-        typedef int16_t property_type;
-
-        Int16Property(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+        Int8Property(std::string name, std::string group, Getter&& getter, Setter&& setter);
     };
 
-    class Int32Property : public BaseProperty<int32_t>
+    class Int16Property : public IntegerProperty<int16_t>
     {
     public:
-        typedef int32_t property_type;
-
-        Int32Property(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+        Int16Property(std::string name, std::string group, Getter&& getter, Setter&& setter);
     };
 
-    class Int64Property : public BaseProperty<int64_t>
+    class Int32Property : public IntegerProperty<int32_t>
     {
     public:
-        typedef int64_t property_type;
-
-        Int64Property(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+        Int32Property(std::string name, std::string group, Getter&& getter, Setter&& setter);
     };
 
-    class UInt8Property : public BaseProperty<uint8_t>
+    class Int64Property : public IntegerProperty<int64_t>
     {
     public:
-        typedef uint8_t property_type;
-
-        UInt8Property(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+        Int64Property(std::string name, std::string group, Getter&& getter, Setter&& setter);
     };
 
-    class UInt16Property : public BaseProperty<uint16_t>
+    class UInt8Property : public IntegerProperty<uint8_t>
     {
     public:
-        typedef uint16_t property_type;
-
-        UInt16Property(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+        UInt8Property(std::string name, std::string group, Getter&& getter, Setter&& setter);
     };
 
-    class UInt32Property : public BaseProperty<uint32_t>
+    class UInt16Property : public IntegerProperty<uint16_t>
     {
     public:
-        typedef uint32_t property_type;
-
-        UInt32Property(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+        UInt16Property(std::string name, std::string group, Getter&& getter, Setter&& setter);
     };
 
-    class UInt64Property : public BaseProperty<uint64_t>
+    class UInt32Property : public IntegerProperty<uint32_t>
     {
     public:
-        typedef uint64_t property_type;
+        UInt32Property(std::string name, std::string group, Getter&& getter, Setter&& setter);
+    };
 
-        UInt64Property(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+    class UInt64Property : public IntegerProperty<uint64_t>
+    {
+    public:
+        UInt64Property(std::string name, std::string group, Getter&& getter, Setter&& setter);
     };
 
     class FloatProperty : public BaseProperty<float>
@@ -104,7 +146,14 @@ namespace property {
     public:
         typedef float property_type;
 
-        FloatProperty(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+        FloatProperty(std::string name, std::string group, Getter&& getter, Setter&& setter);
+
+    private:
+        virtual std::string DoToString() const;
+        virtual void DoFromString(const std::string& string_value);
+    
+        virtual size_t DoSerialize(boost::archive::binary_oarchive& archive) const;
+        virtual void DoDeserialize(boost::archive::binary_iarchive& archive, size_t length);
     };
 
     class BoolProperty : public BaseProperty<bool>
@@ -112,7 +161,14 @@ namespace property {
     public:
         typedef bool property_type;
 
-        BoolProperty(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+        BoolProperty(std::string name, std::string group, Getter&& getter, Setter&& setter);
+
+    private:
+        virtual std::string DoToString() const;
+        virtual void DoFromString(const std::string& string_value);
+    
+        virtual size_t DoSerialize(boost::archive::binary_oarchive& archive) const;
+        virtual void DoDeserialize(boost::archive::binary_iarchive& archive, size_t length);
     };
 
     class StringProperty : public BaseProperty<std::string>
@@ -120,7 +176,14 @@ namespace property {
     public:
         typedef std::string property_type;
 
-        StringProperty(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+        StringProperty(std::string name, std::string group, Getter&& getter, Setter&& setter);
+
+    private:
+        virtual std::string DoToString() const;
+        virtual void DoFromString(const std::string& string_value);
+    
+        virtual size_t DoSerialize(boost::archive::binary_oarchive& archive) const;
+        virtual void DoDeserialize(boost::archive::binary_iarchive& archive, size_t length);
     };
 
     class StringListProperty : public BaseProperty<StringList>
@@ -128,7 +191,7 @@ namespace property {
     public:
         typedef StringList property_type;
 
-        StringListProperty(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+        StringListProperty(std::string name, std::string group, Getter&& getter, Setter&& setter);
         ~StringListProperty();
 
     private:
@@ -144,7 +207,7 @@ namespace property {
     public:
         typedef std::wstring property_type;
 
-        UStringProperty(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+        UStringProperty(std::string name, std::string group, Getter&& getter, Setter&& setter);
     };
 
     class Vector2Property : public BaseProperty<glm::vec2>
@@ -152,7 +215,7 @@ namespace property {
     public:
         typedef glm::vec2 property_type;
 
-        Vector2Property(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+        Vector2Property(std::string name, std::string group, Getter&& getter, Setter&& setter);
     };
 
     class Vector3Property : public BaseProperty<glm::vec3>
@@ -160,7 +223,7 @@ namespace property {
     public:
         typedef glm::vec3 property_type;
 
-        Vector3Property(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+        Vector3Property(std::string name, std::string group, Getter&& getter, Setter&& setter);
     };
 
     class QuaternionProperty : public BaseProperty<glm::quat>
@@ -168,7 +231,7 @@ namespace property {
     public:
         typedef glm::quat property_type;
 
-        QuaternionProperty(std::string name, std::string label, std::string description, std::string group, Getter&& getter, Setter&& setter);
+        QuaternionProperty(std::string name, std::string group, Getter&& getter, Setter&& setter);
     };
 }}
 
