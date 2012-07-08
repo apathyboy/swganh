9293664b885906beef380ac5d8f085bbafc13399
Eric Barr <eb@anhstudios.com>
2012-07-08 12:59:35 -0400

    Initial interfaces for the template manager/factory

--- src/swganh/object/object_template_manager.cc
+++ src/swganh/object/object_template_manager.cc
@@ -3,7 +3,78 @@
 
 #include "anh/resource/resource_manager.h"
 
+#include "swganh/tre/readers/object_template_reader.h"
+
 #include "object_template.h"
 
-using swganh::object::ObjectTemplate;
-using swganh::object::ObjectTemplateManager;
+using namespace swganh::object;
+using swganh::tre::readers::ObjectTemplateReaderV2;
+
+
+void ObjectTemplateFactory::AddTemplateCreator(std::string name, ObjectTemplateCreator&& creator)
+{
+    auto find_iter = object_template_creators_.find(name);
+
+    if (find_iter == std::end(object_template_creators_))
+    {
+        object_template_creators_.insert(find_iter, std::make_pair(name, std::move(creator)));
+    }
+    else
+    {
+        find_iter->second = std::move(creator);
+    }
+}
+
+void ObjectTemplateFactory::RemoveTemplateCreator(const std::string& name)
+{
+    object_template_creators_.erase(name);
+}
+
+std::unique_ptr<ObjectTemplate> ObjectTemplateFactory::CreateObjectTemplate(const std::string& name)
+{
+    auto find_iter = object_template_creators_.find(name);
+
+    if (find_iter != std::end(object_template_creators_))
+    {
+        return find_iter->second();
+    }
+
+    return nullptr;
+}
+
+
+ObjectTemplateManager::ObjectTemplateManager(anh::resource::ResourceManager* resource_manager)
+    : resource_manager_(resource_manager)
+{}
+
+ObjectTemplate* ObjectTemplateManager::GetObjectTemplate(const std::string& name)
+{
+    ObjectTemplate* object_template = nullptr;
+
+    auto find_iter = object_templates_.find(name);
+
+    if (find_iter == std::end(object_templates_))
+    {
+        object_template = CreateObjectTemplate(name);
+    }
+    else
+    {
+        object_template = find_iter->second.get();
+    }
+
+    return object_template;
+}
+
+ObjectTemplate* ObjectTemplateManager::CreateObjectTemplate(const std::string& name)
+{
+    auto resource = resource_manager_->GetHandle(name);
+
+    if (resource)
+    {
+        ObjectTemplateReaderV2 reader(resource);
+
+        std::unique_ptr<ObjectTemplate> object_template;
+    }
+
+    return nullptr;
+}
--- src/swganh/object/object_template_manager.h
+++ src/swganh/object/object_template_manager.h
@@ -4,6 +4,8 @@
 #ifndef SWGANH_OBJECT_OBJECT_TEMPLATE_MANAGER_H_
 #define SWGANH_OBJECT_OBJECT_TEMPLATE_MANAGER_H_
 
+#include <functional>
+#include <memory>
 #include <string>
 #include <unordered_map>
 
@@ -16,9 +18,37 @@ namespace swganh {
 namespace object {
 
     class ObjectTemplate;
+    
+    typedef std::function<std::unique_ptr<ObjectTemplate> ()> ObjectTemplateCreator;
+
+    class ObjectTemplateFactory
+    {
+    public:
+        void AddTemplateCreator(std::string name, ObjectTemplateCreator&& creator);
+        void RemoveTemplateCreator(const std::string& name);
+        std::unique_ptr<ObjectTemplate> CreateObjectTemplate(const std::string& name);
+        
+    private:
+        typedef std::unordered_map<std::string, ObjectTemplateCreator> ObjectTemplateCreatorMap;
+        ObjectTemplateCreatorMap object_template_creators_;
+    };
 
     class ObjectTemplateManager
-    {};
+    {
+    public:
+        ObjectTemplateManager(anh::resource::ResourceManager* resource_manager);
+
+        ObjectTemplate* GetObjectTemplate(const std::string& name);
+
+    private:
+        typedef std::unordered_map<std::string, std::unique_ptr<ObjectTemplate>> ObjectTemplateMap;
+        
+        ObjectTemplate* CreateObjectTemplate(const std::string& name);
+
+        anh::resource::ResourceManager* resource_manager_;
+        ObjectTemplateMap object_templates_;
+        ObjectTemplateFactory object_template_factory_;
+    };
 
 }}
 
