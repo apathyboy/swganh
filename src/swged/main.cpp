64fdfdfa03c9e48e38fa4ea64d7a7cf859b492d9
Eric Barr <eric.barr@ericscottbarr.com>
2013-06-30 11:37:32 -0700

    Adding resources and organizing custom widgets

--- src/swged/main.cpp
+++ src/swged/main.cpp
@@ -1,13 +1,28 @@
 
 #include <QApplication>
-#include "main_window.h"
+#include <QSplashScreen>
+
+#include "widgets/main_window.h"
+
+QSplashScreen* splash = 0;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    QSplashScreen splash(QPixmap(":/images/splash_logo.jpg"));
+    splash.setFont( QFont("Helvetica", 10) );
+    splash.show();
+    
+	qApp->processEvents();
+    splash.showMessage("Loading project");
+    
+	qApp->processEvents();
     MainWindow mw;
     mw.show();
+    
+	qApp->processEvents();
+    splash.finish(&mw);
 
     return a.exec();
 }
