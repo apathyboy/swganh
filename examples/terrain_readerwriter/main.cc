4fe76bcfde9bea1458c772de94bbe5811896b270
Eric Barr <ericbarrjr@me.com>
2013-08-04 20:48:26 -0700

    Added a generic iff struct and reader/writer.

---
+++ examples/terrain_readerwriter/main.cc
@@ -0,0 +1,97 @@
+// This file is part of SWGANH which is released under the MIT license.
+// See file LICENSE or go to http://swganh.com/LICENSE
+
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "swganh/tre/iff/iff.h"
+
+void read_terrain(std::string terrain_filename);
+void print_iff_nodes(swganh::tre::iff_node* head, int depth = 0);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <path to terrain file>" << std::endl;
+        exit(0);
+    }
+    
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+	read_terrain(argv[1]);
+
+    auto stop_time = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Duration: " << 
+        std::chrono::duration_cast<std::chrono::milliseconds>
+            (stop_time - start_time).count() << "ms" << std::endl;
+
+    return 0;
+}
+
+void read_terrain(std::string terrain_filename)
+{
+	std::ifstream in(terrain_filename, std::ios::binary);
+
+	if (!in.is_open())
+	{
+		std::cout << "Invalid filename given: " << terrain_filename << std::endl;
+		return;
+	}
+	
+	std::vector<char> tmp { std::istreambuf_iterator<char>(in), std::istreambuf_iterator<char>() };
+
+	in.close();
+
+	swganh::ByteBuffer data(reinterpret_cast<unsigned char*>(tmp.data()), tmp.size());
+
+	auto iff_head = swganh::tre::parse_iff(data);
+	
+	if (iff_head)
+	{
+		print_iff_nodes(iff_head.get());
+
+		std::ofstream out(terrain_filename + ".alt", std::ios::binary);
+		if (!out.is_open())
+		{
+			std::cout << "Invalid filename given: " << terrain_filename + ".alt" << std::endl;
+			return;
+		}
+
+		swganh::ByteBuffer out_buffer;
+		swganh::tre::write_iff(out_buffer, iff_head.get());
+
+		if (out_buffer.size() > 0)
+		{
+			out.write(reinterpret_cast<char*>(out_buffer.data()), out_buffer.size());
+		}
+
+		out.close();
+	}
+}
+
+void print_iff_nodes(swganh::tre::iff_node* head, int depth)
+{
+	for (int i = 0; i < depth; ++i)
+	{
+		std::cout << " ";
+	}
+
+	std::cout << head->str_name();
+
+	if (head->form)
+	{
+		std::cout << " - " << head->str_form();
+	}
+
+	std::cout << " - (" << head->children.size() << ") " << head->size << "\n";
+
+	for (const auto& child : head->children)
+	{
+		print_iff_nodes(child.get(), depth + 1);
+	}
+}
--- src/swganh/tre/iff/iff.h
+++ src/swganh/tre/iff/iff.h
@@ -2,21 +2,50 @@
 // See file LICENSE or go to http://swganh.com/LICENSE
 #pragma once
 
-#include <stdio.h>
-#include <vector>
-#include <map>
-#include <cstdint>
+#include <iostream>
+#include <list>
 #include <memory>
-#include <functional>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
 #include <swganh/byte_buffer.h>
 #include <swganh/tre/visitors/visitor_interface.h>
 
 
-namespace swganh
-{
-namespace tre
-{
+namespace boost {
+	namespace archive {
+		class binary_iarchive;
+	}
+}
+
+namespace swganh {
+namespace tre {
+
+	struct iff_node
+	{
+		uint32_t name;
+		uint32_t size;
+		uint8_t* data;
+		uint32_t form;
+		iff_node* parent;
+		std::vector<std::unique_ptr<iff_node>> children;
+
+		iff_node()
+			: name(0)
+			, size(0)
+			, data(nullptr)
+			, form(0)
+			, parent(nullptr)
+		{}
+
+		std::string str_name() { return std::string(reinterpret_cast<char*>(&name), sizeof(name)); }
+		std::string str_form() { return std::string(reinterpret_cast<char*>(&form), sizeof(form)); }
+	};
+
+	std::unique_ptr<iff_node> parse_iff(ByteBuffer& resource, iff_node* parent = nullptr);
+	void write_iff(ByteBuffer& resource, iff_node* node);
+
 	class iff_file
 	{
 	public:
@@ -29,5 +58,54 @@ namespace tre
 		static bool isFolderNode_(std::string& name);
 		static std::string getIFFName_(swganh::ByteBuffer& input);
 	};
-}
-}
+
+	class IffReader
+	{
+	public:
+		typedef std::runtime_error BadFileFormat;
+		typedef std::invalid_argument InvalidFormType;
+		typedef std::invalid_argument InvalidRecordType;
+
+		struct Node
+		{
+			char name[4];
+			char type[4];
+			uint32_t size;
+			Node* parent;
+			std::vector<char> data;
+			std::vector<std::unique_ptr<Node>> children;
+
+			/**
+			* Returns the first form found with the requested name.
+			*
+			* This method throws an exception when a requested name cannot be found making
+			* it ideal for daisy-chaining requests:
+			*
+			* @code    node->Form("SCOT")->Form("STAT")->Form("SHOT")->Record("DERV");
+			*
+			* @throws InvalidFormType When no form by the requested name can be found.
+			*/
+			Node* Form(const std::string& form_name);
+			std::list<Node*> FindAllForms(const std::string& form_name);
+
+			Node* Record(const std::string& record_name);
+			std::list<Node*> FindAllRecords(const std::string& record_name);
+		};
+
+		IffReader(const std::vector<char>& input);
+
+		Node* Head();
+		Node* Form(const std::string& form_name);
+		std::list<Node*> FindAllForms(const std::string& form_name);
+		std::list<Node*> FindAllRecords(const std::string& record_name);
+
+	private:
+		IffReader();
+
+		void ReadHead_(boost::archive::binary_iarchive& archive);
+		void ReadNodes_(boost::archive::binary_iarchive& archive, Node* parent);
+
+		std::unique_ptr<Node> head_;
+	};
+
+}}  // namespace swganh::tre
