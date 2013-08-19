2fd5a6582479628693510e7d27198c0cf009f88c
Eric Barr <eb@apathystudios.com>
2013-08-19 07:54:59 -0700

    Added a dark theme

--- src/swged/main.cpp
+++ src/swged/main.cpp
@@ -1,5 +1,6 @@
 
 #include <QApplication>
+#include <QFile>
 #include <QSplashScreen>
 
 #include "widgets/main_window.h"
@@ -18,12 +19,18 @@ int main(int argc, char *argv[])
     QSplashScreen splash(QPixmap(":/images/splash_logo.jpg"));
     splash.setFont( QFont("Helvetica", 10) );
     splash.show();
-    
+
     splash.showMessage("Loading project", Qt::AlignBottom | Qt::AlignLeft, Qt::white);
     app.processEvents();
 
     MainWindow mw;
-    
+
+    // Set stylesheet
+    QFile file(":/style.qss");
+    file.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(file.readAll());
+    app.setStyleSheet(styleSheet);
+
     auto future = mw.openProject();
 
     while (!future.isFinished())
